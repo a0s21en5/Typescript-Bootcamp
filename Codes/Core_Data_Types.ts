ab17b06df77a1ce41fb7c13986db292ae017eaab
Ashish Sahu <ashsahu9713@gmail.com>
2024-03-03 23:00:56 +0530

    Add Some Files

---
+++ Codes/Core_Data_Types.ts
@@ -0,0 +1,10 @@
+let num:number = 10;
+
+let str:string = "Ash";
+
+let isTrue:boolean = false;
+
+
+console.warn(num);
+console.warn(str);
+console.warn(isTrue);
---
+++ Codes/Object_Type.ts
@@ -0,0 +1,18 @@
+const user: object = {
+    name: "Ash",
+    age: 23,
+}
+
+console.warn(user);
+
+type Users = {
+    name: string,
+    age: number
+}
+
+const users: Users = {
+    name: "Ash",
+    age: 23,
+}
+
+console.warn(users);
